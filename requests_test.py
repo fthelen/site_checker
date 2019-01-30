42e3a3255053bb4bac42667a3f37a00638cacacd
fthelen <46471530+fthelen@users.noreply.github.com>
2019-01-30 15:57:06 -0800

    Changed export sequence to add the results list as a new column in df

--- requests_test.py
+++ requests_test.py
@@ -8,15 +8,18 @@ url_df = pandas.read_csv("./url.csv")
 # Converting the 'url' column series into a list to run through loop
 test_list = url_df['url'].tolist()
 
-# Generating a new list full of True/False results from 
+# Generating a new list full of True/False results
 results_list = []
 for x in test_list:  
     r = requests.get(x)
     n = r.status_code == requests.codes.ok
     results_list.append(str(n))
 
-# Converting the results list into a pandas dataframe to use the .to_csv to generate a usable file
-export_df = pandas.DataFrame(data=results_list)
+# Convert results_list into a pandas series to insert into final dataframe
+results_series = pandas.Series(results_list)
+
+# Inserting the results_series into a new dataframe column named 'resutls'
+url_df.insert(loc=1,column='results',value=results_series)
 
 # Sending results to a file named 'siteresults.csv' in the same location as this script
-export_df.to_csv("./siteresults.csv",sep=',',index=False)
+url_df.to_csv("./siteresults.csv",sep=',',index=False)
