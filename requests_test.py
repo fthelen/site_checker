1d005e61480a012c3d2b3cf664909d3d45374792
fthelen <46471530+fthelen@users.noreply.github.com>
2019-01-29 17:01:56 -0800

    Adding pandas

--- requests_test.py
+++ requests_test.py
@@ -1,12 +1,14 @@
 # The goal of this branch is to develop a method for getting one off site data required to answer the status questions
 # This will later need to be itterated through to produce a large list of results from a sites dataset
 
+import pandas
 import requests
-url_list = ['http://google.com', 'http://amazon.com', 'http://fanfiction.net']
+
+url_list = pandas.read_csv(r'C:\Users\Thele\Documents\GitHub\site_checker\sites_list.csv')
 
 results_list = []
-for x in url_list:  
-    r = requests.get(x)
+for row in url_list:  
+    r = requests.get(row)
     n = r.status_code == requests.codes.ok
     results_list.append(str(n))
 print(results_list)
