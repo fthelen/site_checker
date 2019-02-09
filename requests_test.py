6ca374ccdc01717e1a68a271bda283406acbb56c
fthelen <46471530+fthelen@users.noreply.github.com>
2019-02-09 02:14:28 -0800

    Added new apply method and progress bar

--- requests_test.py
+++ requests_test.py
@@ -2,37 +2,37 @@
 import time
 
 # Defines the initial start time
-start_time = time.time()
+# start_time = time.time()
 
 # Importing core packages
 import pandas as pd
 import numpy as np
 import requests
+from requests_futures.sessions import FuturesSession
+from tqdm import tqdm
+
+# Adding pandas progress bar
+tqdm.pandas(desc="progress bar")
 
 # Importing a .csv file named 'url.csv' from the same location as this script
 url_df = pd.read_csv("./url.csv")
 
-# Converting the 'url' column series into a list to run through loop
-url_test_list = url_df['url'].tolist()
-
-# Generating a new list full of True/False results
-url_test_results = []
-for x in url_test_list:
+# Defining functions to apply to url_df
+def get_status_code(url):
     try:
-# The request will produce a timeout error after 5 seconds
-        r = requests.get(x, timeout=5)
-        n = r.status_code == requests.codes.ok
-        url_test_results.append(str(n))
-# This will catch all connection errors and and append them into the list    
-    except requests.exceptions.RequestException as r:
-         n = r == requests.codes.ok
-         url_test_results.append(str(n))
+        r = requests.head(url, timeout=1)
+        return r.status_code == requests.codes.ok
+    except requests.exceptions.RequestException:
+        return False
 
-# Adding results to new column in url_df using np.array()
-url_df['url_test_results'] = pd.DataFrame(np.array(url_test_results))
+# Applying functions
+if __name__ == "__main__":
+        url_df["url_test_results"] = url_df["url"].progress_apply(get_status_code)
+else:
+        print("Error!")
 
 # Sending results to a file named 'siteresults.csv' in the same location as this script
 url_df.to_csv("./results.csv",sep=',',index=False)
 
 # Prints difference between time at end and time at begining i.e. total process time
-print("--- %s seconds ---" % (time.time() - start_time))
+# print("--- %s seconds ---" % (time.time() - start_time))
