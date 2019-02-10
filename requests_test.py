ac1e4daa4b03d3e9417ffbebcb6a3336afeea55e
fthelen <46471530+fthelen@users.noreply.github.com>
2019-02-09 17:31:10 -0800

    Added progress bar, new packages, many improvements

--- requests_test.py
+++ requests_test.py
@@ -2,7 +2,7 @@
 import time
 
 # Defines the initial start time
-# start_time = time.time()
+start_time = time.time()
 
 # Importing core packages
 import pandas as pd
@@ -10,9 +10,10 @@ import numpy as np
 import requests
 from requests_futures.sessions import FuturesSession
 from tqdm import tqdm
+import matplotlib.pyplot as plt
 
 # Adding pandas progress bar
-tqdm.pandas(desc="progress bar")
+tqdm.pandas(desc="progress")
 
 # Importing a .csv file named 'url.csv' from the same location as this script
 url_df = pd.read_csv("./url.csv")
@@ -20,19 +21,23 @@ url_df = pd.read_csv("./url.csv")
 # Defining functions to apply to url_df
 def get_status_code(url):
     try:
-        r = requests.head(url, timeout=1)
-        return r.status_code == requests.codes.ok
+        r = requests.get(url, timeout=10)
+        return r.status_code 
     except requests.exceptions.RequestException:
-        return False
+        return "Exception"
 
 # Applying functions
 if __name__ == "__main__":
-        url_df["url_test_results"] = url_df["url"].progress_apply(get_status_code)
+        url_df["status_code"] = url_df["url"].progress_apply(get_status_code)
 else:
         print("Error!")
 
 # Sending results to a file named 'siteresults.csv' in the same location as this script
 url_df.to_csv("./results.csv",sep=',',index=False)
 
+# Makes a summary_df
+summary_df = url_df.groupby('status_code')['url'].count().reset_index()
+print(summary_df)
+
 # Prints difference between time at end and time at begining i.e. total process time
-# print("--- %s seconds ---" % (time.time() - start_time))
+print("--- %f minutes ---" % ((time.time() - start_time)/60))
